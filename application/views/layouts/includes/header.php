5afd83296973854af7d5d03cf84e750cf3e1c53b
C.K. Chan <chanchikin@ied.edu.hk>
2015-08-21 17:17:13 +0800

    view and layout 17:56

--- application/views/layouts/includes/header.php
+++ application/views/layouts/includes/header.php
@@ -61,67 +61,11 @@
     <div class="container">
 	<div class="row">
 		<div class="col-md-4">
-			<div class="cart-block">
-					<form action="cart/update" method="post">
-						<table cellpadding="6" cellspacing="1" style="width:100%" border="0">
-							<tr>
-								<th>QTY</th>
-								<th>Item Description</th>
-								<th style="text-align:right">Item Price</th>
-							</tr>
-							<tr>
-								<td>QTY</td>
-								<td class="right"><strong>Total</strong></td>
-								<td class="right" style="text-align:right">$</th>
-							</tr>
-						</table>
-						<br>
-						<p><button class="btn btn-default" type="submit">Update Cart</button>
-						<a class="btn btn-default" href="cart">Go To Cart</a></p>
-					</form>
-				</div>
-				
-				<div class="panel panel-default panel-list">
-  <div class="panel-heading panel-heading-dark">
-    <h3 class="panel-title">
-    Categories
-    </h3>
-  </div>
-
-    <ul class="list-group">
-						<li class="list-group-item"><a href="#">Gaming Consoles</a></li>
-						<li class="list-group-item"><a href="#">Xbox One Games</a></li>
-						<li class="list-group-item"><a href="#">Playstation 4 Games</a></li>
-						<li class="list-group-item"><a href="#">Nintendo Wii U Games</a></li>
-						<li class="list-group-item"><a href="#">Xbox 360 Games</a></li>
-						<li class="list-group-item"><a href="#">Playstation 3 Games</a></li>
-						<li class="list-group-item"><a href="#">Nintendo Wii Games</a></li>
-						<li class="list-group-item"><a href="#">Other Console Games</a></li>
-					</ul>
-
-</div>
-	<div class="panel panel-default panel-list">
-					<div class="panel-heading panel-heading-dark">
-						<h3 class="panel-title">
-							Most popular
-						</h3>
-					</div>
-					<!--List group-->
-					<ul class="list-group">
-						<li class="list-group-item"><a href="#">Gaming Consoles</a></li>
-						<li class="list-group-item"><a href="#">Xbox One Games</a></li>
-						<li class="list-group-item"><a href="#">Playstation 4 Games</a></li>
-						<li class="list-group-item"><a href="#">Nintendo Wii U Games</a></li>
-						<li class="list-group-item"><a href="#">Xbox 360 Games</a></li>
-						<li class="list-group-item"><a href="#">Playstation 3 Games</a></li>
-						<li class="list-group-item"><a href="#">Nintendo Wii Games</a></li>
-						<li class="list-group-item"><a href="#">Other Console Games</a></li>
-					</ul>
-				</div>
+			<?php $this->load->view('layouts/includes/sidebar')?>
 		</div>
 		<div class="col-md-8">
 			<div class="panel panel-default">
 					<div class="panel-heading panel-heading-green">
-						<h3 class="panel-title">Latest Releases</h3>
+						<h3 class="panel-title">The Gaming Place</h3>
 					</div>
 					<div class="panel-body">
