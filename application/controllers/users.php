af925306e81917002fc7305c73efc0ced1f76bcc
C.K. Chan <chanchikin@ied.edu.hk>
2015-08-25 16:58:07 +0800

    user 5:24

---
+++ application/controllers/users.php
@@ -0,0 +1,13 @@
+<?php 
+class Users extends CI_Controller{
+	public function register(){
+		$data['main_content']='register';
+			
+		$this->load->view('layouts/main',$data);
+	}
+	
+	
+}
+
+
+?>
---
+++ application/views/register.php
@@ -0,0 +1,29 @@
+	<?php echo validation_errors(); ?>
+	
+	<form method = "post" acton="<?php echo base_url();?>users/register">
+							<div class="form-group">
+								<label>First Name*</label>
+								<input type="text" class="form-control" name ="first_name" placeholder="Enter your first name"/ >
+							</div>
+							<div class="form-group">
+								<label>Last Name*</label>
+								<input type="text" class="form-control" name ="last_name" placeholder="Enter your last name"/ >
+							</div>
+							<div class="form-group">
+								<label>Email Address*</label>
+								<input type="text" class="form-control" name ="email" placeholder="Enter Your Email Address"/ >
+							</div>
+							<div class="form-group">
+								<label>Choose Username*</label>
+								<input type="text" class="form-control" name ="username" placeholder="Enter Your Username"/ >
+							</div>
+							<div class="form-group">
+								<label>Choose Password*</label>
+								<input type="password" class="form-control" name ="password" placeholder="Enter Your Password"/ >
+							</div>
+							<div class="form-group">
+								<label>Confirm Password*</label>
+								<input type="password" class="form-control" name ="password2" placeholder="Enter Your Password Again"/ >
+							</div>
+							<button type="submit" class="btn btn-primary">Register</button>
+						</form>
